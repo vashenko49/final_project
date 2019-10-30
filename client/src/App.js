a9eb3605208f01828bd64daf30055a91c464dfb3
Udaff <udaffchik@gmail.com>
2019-10-30 19:28:17 +0100

    connecting materialize

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import "./../node_modules/materialize-css/dist/css/materialize.min"
+import materialize from './../node_modules/materialize-css/dist/js/materialize.min'
 import './App.css';
 import Product from './../src/components/Product/Product.js'
 function App() {
