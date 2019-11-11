c287636f1570d558e1a1c1befe9bb2a4689cb8dc
Sergey <vashenko49@gmail.com>
2019-11-11 19:05:44 +0200

    fixed

--- server/server.js
+++ server/server.js
@@ -10,18 +10,13 @@ connectDB();
 //init middleware
 app.use(express.json({extended: false}));
 
-//Routes
-
-app.use('/api/users', require('./routes/users'));
-app.use('/api/auth', require('./routes/auth'));
 
 
-if(process.env.NODE_ENV==="production"){
-    app.use(express.static('client/build'));
-    app.get('*',(req,res)=>{
-        res.sendFile(path.resolve(__dirname,'../','client/build/index.html'))
-    })
-}
+app.use(express.static('../client/build'));
+app.get('*',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'../','client/build/index.html'))
+});
+
 
 const PORT = process.env.PORT || 5000;
 
