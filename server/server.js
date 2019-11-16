23486fd84359fbd8b6826311f65b7f97d35831ec
Sergey <vashenko49@gmail.com>
2019-11-16 22:12:08 +0200

    eliminated the threat of application data leakage

--- server/server.js
+++ server/server.js
@@ -3,36 +3,46 @@ const connectDB = require('./config/db');
 const bodyParser = require('body-parser');
 const passport = require('passport');
 const path = require('path');
+const getConfig = require('./config/GetConfig');
 
 
 const app = express();
 
 // Body parser middleware
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
 //connect data base
-connectDB();
+getConfig('configs-v1')
+  .then(() => {
+    // connectDB();
+    connectDB(process.env.urlDataBase);
+    app.use(passport.initialize());
+    require("./config/passport")(passport);
+  }).catch(err => {
+  console.error(err);
+});
+
+
+app.use(bodyParser.json());
+
 
 // Passport middleware
-app.use(passport.initialize());
 
 // Passport Config
-require("./config/passport")(passport);
 
 // Use Routes
-app.use('/oauth',require('./routes/oauth'));
-
+app.use('/oauth', require('./routes/oauth'));
+app.use('/configs', require('./routes/configs'));
 
 
 app.use(express.static('../client/build'));
-app.get('*',(req,res)=>{
-    res.sendFile(path.resolve(__dirname,'../','client/build/index.html'))
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../', 'client/build/index.html'))
 });
 
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`server start on ${PORT}`);
+  console.log(`server start on ${PORT}`);
 });
