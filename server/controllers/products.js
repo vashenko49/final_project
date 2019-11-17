7fc2e954672f1d8403f645228da5e14799d986aa
iamr0man <koriny4@gmail.com>
2019-11-17 20:21:56 +0200

    add form validation and beautify code

--- server/controllers/products.js
+++ server/controllers/products.js
@@ -6,29 +6,39 @@ const rand = uniqueRandom(0, 999999);
 const queryCreator = require("../common/queryCreator");
 const _ = require("lodash");
 
+const { validationResult } = require('express-validator');
+
 // *TODO*
 exports.addImages = (req, res, next) => {
-  
+
 };
 
 exports.addProduct = async (req, res, next) => {
+
+  const errors = validationResult(req)
+
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() })
+  }
+
   const productFields = _.cloneDeep(req.body);
 
   productFields.itemNo = rand();
 
   productFields.name = productFields.name
-      .toLowerCase()
-      .trim()
-      .replace(/\s\s+/g, " "); //replace few whitespace
-    
+    .toLowerCase()
+    .trim()
+    .replace(/\s\s+/g, " "); //replace few whitespace
+
 
   const updatedProduct = queryCreator(productFields);
+
   try {
 
     const newProduct = new Product(updatedProduct);
 
     const product = await newProduct.save()
-    
+
     res.status(200).json(product)
 
   } catch (err) {
@@ -39,59 +49,66 @@ exports.addProduct = async (req, res, next) => {
 };
 
 exports.updateProduct = async (req, res, next) => {
+
+  const errors = validationResult(req)
+
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() })
+  }
+
   try {
 
     let product = await Product.findOne({ _id: req.params.id })
-    
+
     if (!product) {
-          return res.status(500).json({
-            message: `Product with id "${req.params.id}" is not found.`
-          });
-      } 
-          
+      return res.status(500).json({
+        message: `Product with id "${req.params.id}" is not found.`
+      });
+    }
+
     const productFields = _.cloneDeep(req.body);
 
     productFields.name = productFields.name
-          .toLowerCase()
-          .trim()
-          .replace(/\s\s+/g, " "); 
-            
+      .toLowerCase()
+      .trim()
+      .replace(/\s\s+/g, " ");
+
     const updatedProduct = queryCreator(productFields);
-  
+
     product = await Product.findOneAndUpdate(
       { _id: req.params.id },
       { $set: updatedProduct },
       { new: true }
     )
-    res.status(200).json({ isUpdated: true }) 
+    res.status(200).json({ isUpdated: true })
   } catch (err) {
     res.status(500).json({
       message: `Error happened on server: "${err}" `
     })
-  }       
+  }
 };
 
 exports.deleteProduct = async (req, res, next) => {
-    try {
-      await Product.findOneAndRemove({ _id: req.params.id }) //don`t know about params.id
+  try {
+    await Product.findOneAndRemove({ _id: req.params.id }) //don`t know about params.id
 
-      res.status(200)json({ isDeleted: true })
+    res.status(200).json({ isDeleted: true })
   } catch (err) {
     res.status(500).json({
       message: `Error happened on server: "${err}" `
-    })  
+    })
   }
 }
 
 exports.getProducts = async (req, res, next) => {
-  try{
+  try {
     const products = await Product.find();
 
     res.status(200).json(products)
   } catch (err) {
     res.status(500).json({
       message: `Error happened on server: "${err}" `
-    })  
+    })
   }
 };
 
@@ -99,15 +116,15 @@ exports.getProductById = async (req, res, next) => {
   try {
     const product = await Product.findOne({ _id: req.params.id })
 
-    if(!product) {
-      return res.status(400).json({ msg: `Product with id ${req.params.id} not found`})
+    if (!product) {
+      return res.status(400).json({ msg: `Product with id ${req.params.id} not found` })
     }
 
     res.status(200).json(product)
   } catch (err) {
     res.status(500).json({
       message: `Error happened on server: "${err}" `
-    })  
+    })
   }
 };
 
--- server/routes/filters.js
+++ server/routes/filters.js
@@ -1,22 +1,40 @@
 const express = require("express")
 const router = express.Router();
 const passport = require("passport");
+const { check } = require('express-validator');
 
 //$TODO$ ADD middleware 
 
 //Import controllers
-const { 
+const {
     createFilter,
     updateFilter,
-    getAllFilters
+    getAllFilters,
+    deleteFilter
 } = require("../controllers/filters")
 
 // @route POST /filters
 // @desc Create new filter
 // @access Private
 router.post(
-    "/", 
+    "/",
+    check('type', 'Type is required')
+        .not()
+        .isEmpty(),
+    check('name', 'Name is required')
+        .not()
+        .isEmpty(),
     createFilter)
+
+// @route PUT /filters/:id
+// @desc Update filter
+// @access Private
+router.put(
+    "/:id",
+    check('name', 'Name is required')
+        .not()
+        .isEmpty(),
+    updateFilter)
 
 // @route GET /filters/all
 // @desc Get all filters
@@ -30,7 +48,7 @@ router.get(
 // @access  Private
 router.delete(
     "/:id",
-    updateFilter
+    deleteFilter
 );
 
 module.exports = router;
