784192241a63b25f9b5ad0ba421a984456bb5bdc
iamr0man <koriny4@gmail.com>
2019-11-17 13:59:05 +0200

    create private route

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,34 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom'
+
+import Login from './components/auth/Login'
+import Registration from './components/auth/Registration'
+
+import PrivateRoute from './components/routing/PrivateRoute';
+
 import './App.scss';
 
-import Container from './components/auth/Container';
 function App() {
-  return <Container />;
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/subscribe">Subscribe</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route exact path="/" component={Registration} />
+          <PrivateRoute exact path="/subscribe" component={Login} />
+        </Switch>
+      </div>
+    </Router>
+  )
 }
 
 export default App;
