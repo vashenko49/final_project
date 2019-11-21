85fc61f2b76da7a63693935a2e16e40a137f583c
Roman Loboda <whiten.ncs@gmail.com>
2019-11-21 09:21:11 +0000

    fix create store

--- client/src/App.js
+++ client/src/App.js
@@ -11,28 +11,30 @@ import PrivateRoute from './components/routing/PrivateRoute';
 import './App.scss';
 import PasswordRecovery from './components/auth/PasswordRecovery';
 
+const store = configureStore();
+
 function App() {
   return (
     <Provider store={store}>
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/subscribe">Subscribe</Link>
-            </li>
-          </ul>
-        </nav>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/subscribe">Subscribe</Link>
+              </li>
+            </ul>
+          </nav>
 
-        {/* A <Switch> looks through its children <Route>s and
+          {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
-        <Switch>
-          <Route exact path="/" component={Registration} />
-          <Route exact path="/passwordrecovery/:token" component={PasswordRecovery} />
-          <PrivateRoute exact path="/subscribe" component={Login} />
-        </Switch>
-      </div>
-    </Router>
+          <Switch>
+            <Route exact path="/" component={Registration} />
+            <Route exact path="/passwordrecovery/:token" component={PasswordRecovery} />
+            <PrivateRoute exact path="/subscribe" component={Login} />
+          </Switch>
+        </div>
+      </Router>
     </Provider>
   );
 }
