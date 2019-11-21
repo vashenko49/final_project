9948a479f63921bf3aa879a436572e760a602646
Roman Loboda <whiten.ncs@gmail.com>
2019-11-21 09:18:01 +0000

    return develop structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import { Provider } from 'react-redux';
-
 import { configureStore } from './store';
 
 import Login from './components/auth/Login';
@@ -10,30 +9,30 @@ import Registration from './components/auth/Registration';
 import PrivateRoute from './components/routing/PrivateRoute';
 
 import './App.scss';
-
-const store = configureStore();
+import PasswordRecovery from './components/auth/PasswordRecovery';
 
 function App() {
   return (
     <Provider store={store}>
-      <Router>
-        <div>
-          <nav>
-            <ul>
-              <li>
-                <Link to="/subscribe">Subscribe</Link>
-              </li>
-            </ul>
-          </nav>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/subscribe">Subscribe</Link>
+            </li>
+          </ul>
+        </nav>
 
-          {/* A <Switch> looks through its children <Route>s and
+        {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
-          <Switch>
-            <Route exact path="/" component={Registration} />
-            <PrivateRoute exact path="/subscribe" component={Login} />
-          </Switch>
-        </div>
-      </Router>
+        <Switch>
+          <Route exact path="/" component={Registration} />
+          <Route exact path="/passwordrecovery/:token" component={PasswordRecovery} />
+          <PrivateRoute exact path="/subscribe" component={Login} />
+        </Switch>
+      </div>
+    </Router>
     </Provider>
   );
 }
