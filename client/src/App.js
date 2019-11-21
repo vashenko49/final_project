2778314525cc8f0d656aef4c23167300b62fadf7
Roman Loboda <whiten.ncs@gmail.com>
2019-11-21 09:12:49 +0000

    init structure for react-redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
+import { configureStore } from './store';
 
 import Login from './components/auth/Login';
 import Registration from './components/auth/Registration';
@@ -7,29 +10,31 @@ import Registration from './components/auth/Registration';
 import PrivateRoute from './components/routing/PrivateRoute';
 
 import './App.scss';
-import PasswordRecovery from './components/auth/PasswordRecovery';
+
+const store = configureStore();
 
 function App() {
   return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/subscribe">Subscribe</Link>
-            </li>
-          </ul>
-        </nav>
+    <Provider store={store}>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/subscribe">Subscribe</Link>
+              </li>
+            </ul>
+          </nav>
 
-        {/* A <Switch> looks through its children <Route>s and
+          {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
-        <Switch>
-          <Route exact path="/" component={Registration} />
-          <Route exact path="/passwordrecovery/:token" component={PasswordRecovery} />
-          <PrivateRoute exact path="/subscribe" component={Login} />
-        </Switch>
-      </div>
-    </Router>
+          <Switch>
+            <Route exact path="/" component={Registration} />
+            <PrivateRoute exact path="/subscribe" component={Login} />
+          </Switch>
+        </div>
+      </Router>
+    </Provider>
   );
 }
 
---
+++ client/src/reducers/index.js
@@ -0,0 +1,7 @@
+import { combineReducers } from 'redux';
+
+import adminProducts from './adminProducts';
+
+export default combineReducers({
+  adminProducts
+});
