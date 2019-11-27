efe4fb82e52a3109f441b93ece32bb886cb056b3
iamr0man <koriny4@gmail.com>
2019-11-27 20:15:35 +0200

    create product page

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,11 @@ import { configureStore } from './store';
 
 import Login from './components/auth/Login';
 import Registration from './components/auth/Registration';
+import Bag from './components/cart/Bag'
+import Item from './components/cart/Item'
+import Row from './components/row/Row'
+
+import ProductPage from "./components/page/ProductPage";
 
 import PrivateRoute from './components/routing/PrivateRoute';
 
@@ -18,18 +23,20 @@ function App() {
     <Provider store={store}>
       <Router>
         <div>
-          <nav>
+          {/* <nav>
             <ul>
               <li>
                 <Link to="/subscribe">Subscribe</Link>
               </li>
             </ul>
-          </nav>
+          </nav> */}
+
+          {/* <Row left={<Bag/>} right={<Item/>}/> */}
 
           {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
           <Switch>
-            <Route exact path="/" component={Registration} />
+            <Route exact path="/" component={ProductPage} />
             <Route exact path="/passwordrecovery/:token" component={PasswordRecovery} />
             <PrivateRoute exact path="/subscribe" component={Login} />
           </Switch>
