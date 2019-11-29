6af7269dc9b042f568b0b2165ed2ed330866acad
Roman Loboda <whiten.ncs@gmail.com>
2019-11-29 10:04:28 +0000

    merge develop v2

--- client/src/reducers/adminProducts.js
+++ client/src/reducers/adminProducts.js
@@ -11,11 +11,7 @@ const initState = {
 
 export default function(state = initState, action) {
   switch (action.type) {
-<<<<<<< HEAD
     case PRODUCTS.GET_API_PRODUCTS_REQUEST:
-=======
-    case PRODUCTS.GET_API_REQUEST:
->>>>>>> ad9f27ab884245c9f9dd3e88a7a2d8ab537dfd17
       return {
         ...state,
         ...{
@@ -23,11 +19,7 @@ export default function(state = initState, action) {
         }
       };
 
-<<<<<<< HEAD
     case PRODUCTS.GET_API_PRODUCTS_SUCCEEDED:
-=======
-    case PRODUCTS.GET_API_SUCCEEDED:
->>>>>>> ad9f27ab884245c9f9dd3e88a7a2d8ab537dfd17
       return {
         ...state,
         ...{
@@ -36,11 +28,7 @@ export default function(state = initState, action) {
         }
       };
 
-<<<<<<< HEAD
     case PRODUCTS.GET_API_PRODUCTS_FAILED:
-=======
-    case PRODUCTS.GET_API_FAILED:
->>>>>>> ad9f27ab884245c9f9dd3e88a7a2d8ab537dfd17
       return {
         ...state,
         ...{
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,7 +1,6 @@
 import { combineReducers } from 'redux';
 
 import adminProducts from './adminProducts';
-<<<<<<< HEAD
 import adminBrands from './adminBrands';
 import adminFilters from './adminFilters';
 
@@ -9,9 +8,4 @@ export default combineReducers({
   adminProducts,
   adminBrands,
   adminFilters
-=======
-
-export default combineReducers({
-  adminProducts
->>>>>>> ad9f27ab884245c9f9dd3e88a7a2d8ab537dfd17
 });
