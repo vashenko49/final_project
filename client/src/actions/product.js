065d1f2d87f2b3bb6be36509b7c09d7479573924
Ramazan Khanbabaev <ramazan.khanbabaev@gmail.com>
2019-12-03 16:57:50 +0200

    carousel is done

--- client/src/actions/product.js
+++ client/src/actions/product.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-import { GET_PRODUCT, PRODUCT_ERROR } from '../constants/product';
+import { GET_PRODUCT, GET_PRODUCTS, PRODUCT_ERROR } from '../constants/product';
 // Get current product
 export const getCurrentProduct = productId => async dispatch => {
   try {
@@ -8,6 +8,26 @@ export const getCurrentProduct = productId => async dispatch => {
 
     dispatch({
       type: GET_PRODUCT,
+      payload: res.data
+    });
+  } catch (err) {
+    dispatch({
+      type: PRODUCT_ERROR,
+      payload: {
+        msg: err.response.statusText,
+        status: err.response.status
+      }
+    });
+  }
+};
+
+// Get all products
+export const getProducts = () => async dispatch => {
+  try {
+    const res = await axios.get('/products');
+
+    dispatch({
+      type: GET_PRODUCTS,
       payload: res.data
     });
   } catch (err) {
---
+++ client/src/components/ImageCarousel/ImageCarousel.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import Carousel from 'react-material-ui-carousel';
+import ImageCarouselItem from './ImageCarouselItem';
+import autoBind from 'auto-bind';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import PropTypes from 'prop-types';
+import * as product from '../../actions/product';
+import './ImageCarousel.scss';
+
+class ImageCarousel extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      autoPlay: true,
+      timer: 1000,
+      animation: 'slide',
+      indicators: false
+    };
+
+    autoBind(this);
+  }
+
+  componentDidMount() {
+    this.props.getProducts();
+  }
+
+  render() {
+    const { products } = this.props;
+
+    return (
+      <div style={{ marginTop: '50px', color: '#494949' }}>
+        <Carousel
+          className="image-carousel"
+          autoPlay={this.state.autoPlay}
+          timer={this.state.timer}
+          animation={this.state.animation}
+          indicators={this.state.indicators}
+        >
+          {products.map((item, index) => {
+            return <ImageCarouselItem item={item} key={index} />;
+          })}
+        </Carousel>
+      </div>
+    );
+  }
+}
+
+ImageCarousel.propTypes = {
+  products: PropTypes.arrayOf(PropTypes.object.isRequired)
+};
+
+ImageCarousel.defaultProps = {};
+
+function mapStateToProps(state) {
+  return {
+    products: state.product.products
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    getProducts: bindActionCreators(product.getProducts, dispatch)
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ImageCarousel);
