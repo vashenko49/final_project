a613a322da6e6e7ea4f4253a857e3d1f5a5565d1
alexandrrusanov <Darkelf3001>
2019-12-08 14:30:35 +0200

    minor fixes after merging last changes from develop

--- client/src/services/FooterSubscribeAPI.js
+++ client/src/services/FooterSubscribeAPI.js
@@ -2,6 +2,6 @@ const axios = require('axios');
 
 export default class FooterSubscribeAPI {
   static async addSubscriber(email) {
-    return await axios.post('http://localhost:5000/subscriber', { email });
+    return await axios.post('/subscriber', { email });
   }
 }
