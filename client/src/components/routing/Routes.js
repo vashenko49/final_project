4605a12292c3b4f02e88cf7757a3579a0f9b704c
iamr0man <koriny4@gmail.com>
2020-01-04 13:26:59 +0200

    add not found page

---
+++ client/src/components/routing/Routes.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import AdminPanel from '../admin-panel/AdminPanel';
+import FooterLinkPage from '../FooterLinkPage/FooterLinkPage';
+
+import ProductPageF from '../page/ProductPageF';
+import Cart from '../cart/Cart';
+import MainPage from '../page/MainPage';
+import HeaderSearchProductPage from '../HeaderSearchProductPage/HeaderSearchProductPage';
+import NotFound from '../page/NotFound';
+
+import Authorization from '../Authorization/Authorization';
+import PasswordRecovery from '../Authorization/PasswordRecovery/PasswordRecovery';
+import CatalogPage from '../page/CatalogPage/CatalogPage';
+
+class Routes extends Component {
+  render() {
+    return (
+      <Switch>
+        <Route exact path="/authorization" component={Authorization} />
+        <Route exact path="/links/content/:customId" component={FooterLinkPage} />
+        <Route exact path="/admin-panel*" component={AdminPanel} />
+        <Route exact exect path="/" component={MainPage} />
+        <Route exact exect path="/product/:id" component={ProductPageF} />
+        <Route exact exect path="/catalog/:id" component={CatalogPage} />
+        <Route exact exect path="/products/search" component={HeaderSearchProductPage} />
+        <Route exact exect path="/cart/:id" component={Cart} />
+        <Route exact exect path="/passwordrecovery/:token" component={PasswordRecovery} />
+        <Route component={NotFound} />
+      </Switch>
+    );
+  }
+}
+
+export default Routes;
