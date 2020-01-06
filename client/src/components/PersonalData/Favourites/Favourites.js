f0cd51087c2e4a7a64e4b2d7b8a6bd21ac2d36ba
Sergey <vashenko49@gmail.com>
2020-01-06 22:38:34 +0200

    done favourites

--- client/src/components/PersonalData/Favourites/Favourites.js
+++ client/src/components/PersonalData/Favourites/Favourites.js
@@ -1,8 +1,71 @@
 import React, { Component } from 'react';
+import Grid from '@material-ui/core/Grid';
+import MiniProduct from '../../MiniProduct/MiniProduct';
+import FavouritesAPI from '../../../services/FavouritesAPI';
+import ClearIcon from '@material-ui/icons/Clear';
+
+import './Favourites.scss';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 class Favourites extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      products: [],
+      load: false
+    };
+  }
+
+  componentDidMount() {
+    const { getFavourites } = this;
+    getFavourites();
+  }
+
+  onClickRemove = event => {
+    const { getFavourites } = this;
+    this.setState({ load: true });
+    FavouritesAPI.removeFromFavourites(event.currentTarget.dataset.id).then(() => {
+      getFavourites();
+    });
+  };
+
+  getFavourites = () => {
+    FavouritesAPI.getFavourites().then(res => {
+      this.setState({ products: res.idProduct, load: false });
+    });
+  };
+
   render() {
-    return <div>Favourites</div>;
+    const { onClickRemove } = this;
+    const { products, load } = this.state;
+    return (
+      <div className="favourite-block-container">
+        <Grid container spacing={3} direction="row" alignItems="center">
+          {products.map(product => {
+            const { productUrlImg, filterImg, nameProduct, _id, model } = product;
+            return (
+              <Grid key={_id} item lg={3} md={6} sm={6} xs={12}>
+                <div className="container-product">
+                  <MiniProduct
+                    _id={_id}
+                    nameProduct={nameProduct}
+                    filterImg={filterImg}
+                    productUrlImg={productUrlImg}
+                    model={model}
+                  />
+                  <ClearIcon onClick={onClickRemove} data-id={_id} className="close-icon" />
+                </div>
+              </Grid>
+            );
+          })}
+        </Grid>
+        {load && (
+          <div className="favourite-block">
+            <CircularProgress disableShrink className="favourite-block-preloader" />
+          </div>
+        )}
+      </div>
+    );
   }
 }
 
