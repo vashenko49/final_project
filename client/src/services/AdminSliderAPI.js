0d08c9aa253d3b7b82e9316204cbed1ef6f5e40e
Loboda Roman <whiten.ncs@gmail.com>
2020-01-11 08:42:18 +0000

    pre merge develop

--- client/src/services/AdminSliderAPI.js
+++ client/src/services/AdminSliderAPI.js
@@ -2,23 +2,23 @@ import axios from 'axios';
 
 export default class AdminSliderAPI {
   static getSlider() {
-    return axios.get(`/slider`);
+    return axios.get(`/api/slider`);
   }
 
   static getSliderById(id) {
-    return axios.get(`/slider/${id}`);
+    return axios.get(`/api/slider/${id}`);
   }
 
   static createSlider(body) {
-    return axios.post(`/slider`, body);
+    return axios.post(`/api/slider`, body);
   }
 
   static updateSlider(id, body) {
-    return axios.put(`/slider/${id}`, body);
+    return axios.put(`/api/slider/${id}`, body);
   }
 
   static deleteSlider(id) {
-    return axios.delete(`/slider/${id}`);
+    return axios.delete(`/api/slider/${id}`);
   }
 
   // static changeStatusSlider(id, body) {
