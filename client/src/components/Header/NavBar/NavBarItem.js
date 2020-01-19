ded5821b91d57ac178776a432e122fe000ad1f5d
alexandrrusanov <Darkelf3001>
2020-01-19 19:06:37 +0200

    some fixes

--- client/src/components/Header/NavBar/NavBarItem.js
+++ client/src/components/Header/NavBar/NavBarItem.js
@@ -1,60 +1,86 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 
 import Button from '@material-ui/core/Button';
-import Menu from '@material-ui/core/Menu';
+import MenuList from '@material-ui/core/MenuList';
 import MenuItem from '@material-ui/core/MenuItem';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import ClickAwayListener from '@material-ui/core/ClickAwayListener';
+import Grow from '@material-ui/core/Grow';
+import Paper from '@material-ui/core/Paper';
+import Popper from '@material-ui/core/Popper';
 
-export default class NavBarItem extends Component {
-  state = {
-    anchorEl: null,
-    open: false
+export default function NavBarItem(props) {
+  const [open, setOpen] = React.useState(false);
+  const anchorRef = React.useRef(null);
+
+  const { item, children } = props;
+
+  const handleToggle = () => {
+    setOpen(prevOpen => !prevOpen);
   };
 
-  handleClick = event => {
-    this.setState({ open: true, anchorEl: event.currentTarget });
+  const handleClose = event => {
+    if (anchorRef.current && anchorRef.current.contains(event.target)) {
+      return;
+    }
+
+    setOpen(false);
   };
 
-  handleRequestClose = () => {
-    this.setState({ open: false });
-  };
-
-  render() {
-    const { item, children } = this.props;
-
-    return (
-      <div className="header-navbar-item">
-        <Button
-          aria-owns={this.state.open ? 'simple-menu' : null}
-          aria-haspopup="true"
-          onClick={this.handleClick}
-        >
-          {item.name}
-          <ExpandMoreIcon />
-        </Button>
-        <Menu
-          id="simple-menu"
-          anchorEl={this.state.anchorEl}
-          open={this.state.open}
-          onClick={this.handleRequestClose}
-          style={{
-            top: '40px'
-          }}
-        >
-          {children
-            .filter(item => item.enabled === true)
-            .map((child, index) =>
-              item._id === child.parentId ? (
-                <MenuItem key={index} onClick={this.handleRequestClose}>
-                  <Link className="header-navbar-links" to={{ pathname: `/catalog/${child._id}` }}>
-                    {child.name}
-                  </Link>
-                </MenuItem>
-              ) : null
-            )}
-        </Menu>
-      </div>
-    );
+  function handleListKeyDown(event) {
+    if (event.key === 'Tab') {
+      event.preventDefault();
+      setOpen(false);
+    }
   }
+
+  const prevOpen = React.useRef(open);
+  React.useEffect(() => {
+    if (prevOpen.current === true && open === false) {
+      anchorRef.current.focus();
+  }
+
+    prevOpen.current = open;
+  }, [open]);
+
+  return (
+    <div className="header-navbar-item">
+      <Button
+        ref={anchorRef}
+        aria-controls={open ? 'menu-list-grow' : undefined}
+        aria-haspopup="true"
+        onClick={handleToggle}
+      >
+        {item.name}
+        <ExpandMoreIcon/>
+      </Button>
+      <Popper open={open} anchorEl={anchorRef.current} role={undefined} transition disablePortal>
+        {({ TransitionProps, placement }) => (
+          <Grow
+            {...TransitionProps}
+            style={{ transformOrigin: placement === 'bottom' ? 'center top' : 'center bottom' }}
+          >
+            <Paper>
+              <ClickAwayListener onClickAway={handleClose}>
+                <MenuList autoFocusItem={open} id="menu-list-grow" onKeyDown={handleListKeyDown}>
+                  {children
+                    .filter(item => item.enabled === true)
+                    .map((child, index) =>
+                      item._id === child.parentId ? (
+                        <MenuItem key={index} onClick={handleClose}>
+                          <Link className="header-navbar-links" to={{ pathname: `/catalog/${child._id}`}}>
+                            {child.name}
+                          </Link>
+                        </MenuItem>
+                      ) : null
+                    )}
+                </MenuList>
+              </ClickAwayListener>
+            </Paper>
+          </Grow>
+        )}
+      </Popper>
+    </div>
+  );
 }
